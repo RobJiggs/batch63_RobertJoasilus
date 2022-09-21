2f39346970cd8ba28e4d6ce9583a189c4d7d0bc9
robji <RobertJoasilus@gmail.com>
2022-09-21 10:30:47 -0400

    commit 6

--- Day2_App1/src/java/basics/Swapwithout.java
+++ Day2_App1/src/java/basics/Swapwithout.java
@@ -9,5 +9,17 @@ package java.basics;
  * @date: Sep 21, 2022
  */
 public class Swapwithout {
-
+	static void myMethod(int a, int b) {
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a+" "+b);
+		
+		
+	}
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		myMethod(a,b);
+	}
 }
