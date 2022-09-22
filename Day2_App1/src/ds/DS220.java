ad2038e09a2611c9a67ece03c6905e8e5fb9ce91
robji <RobertJoasilus@gmail.com>
2022-09-22 15:23:03 -0400

    11th commmit

--- Day2_App1/src/ds/DS220.java
+++ Day2_App1/src/ds/DS220.java
@@ -11,26 +11,33 @@ import java.util.Random;
  * @date: Sep 22, 2022
  */
 public class DS220 {
-	int max=500;
-	int min=-500;
+	public static void main(String[] args) {
+		int max=500;
+		int min=-500;
+		
+		Random rand = new Random(); 
+		int int_random = rand.nextInt(max - min) + min;
+		
+		///user number
+		int num=55;
+
+		//put in loop that asks the user to guess again with Scanner.in
+		if(int_random>num) {
+			System.out.println("Your number is too low guess higher");
+		}
+		else if(int_random<num) {
+			System.out.println("Your number is too high guess lower");
+			
+			
+		}
+		else {
+			System.out.println("right on");
+			
+		}
+	}
 	
-	Random rand = new Random(); 
-	int int_random = rand.nextInt(max - min) + min;
 	
-	///user number
-	int num=55;
-	//put in loop that asks the user to guess again with Scanner.in
-	if(int_random>num) {
-		System.out.println("Your number is too low guess higher");
-	}
-	else if(int_random>num) {
-		System.out.println("Your number is too high guess lower");
-		
-		
-	}
-	else {
-		System.out.println("right on");
-		
-	}
+	
+	
 	
 }
